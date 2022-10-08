f009531bd623b3a38ffa1ba81e1a25adbdc73d34
Matt <gitkraken@matt-fellows.me.uk>
2022-10-08 09:23:52 +0100

    Grids

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export default function App() {
   const [letters, setLetters] = useState<string>('');
+  const [word, setWord] = useState<string>('');
+  const [chosenLetterIndexes, setChosenLetterIndexes] = useState<number[]>([]);
+
   useEffect(() => {
     let localLetters = '';
     while (localLetters.length < 25) {
@@ -14,17 +17,32 @@ export default function App() {
     setLetters(localLetters)
   }, [])
 
+
+  const addLetterToWord = (letter: string): void => {
+    setWord(word + letter);
+  }
+
   const renderLetters = () => {
     const grid = [];
     for (let i = 0; i < 25; i++) {
-      grid.push(<div>{letters[i]}</div>)
+      grid.push(<div onClick={() => {
+        if (chosenLetterIndexes.filter(l => l === i).length === 0) { 
+          let newIndexes = [...chosenLetterIndexes];
+          newIndexes.push(i);
+          addLetterToWord(letters[i])
+          setChosenLetterIndexes(newIndexes);
+        }
+      }} className={'gridletter' + (chosenLetterIndexes.filter(l => l === i).length > 0 ? ' chosen' : '')}>{letters[i]}</div>)
     }
     return grid;
   }
 
   return (
-    <main>
-      {renderLetters()}
-    </main>
+    <>
+      <div className='grid'>
+        {renderLetters()}
+      </div>
+      <div>{word}</div>
+    </>
   )
 }
