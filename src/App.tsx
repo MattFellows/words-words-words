2f0181e6d11db5fb7c124575b602e7827a01257c
Matt <gitkraken@matt-fellows.me.uk>
2022-10-08 13:54:17 +0100

    Letter distribution fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,32 @@ export default function App() {
   const [word, setWord] = useState<string>('');
   const [chosenLetterIndexes, setChosenLetterIndexes] = useState<number[]>([]);
 
-  const generateString = () => {
-    let localLetters = '';
-    while (localLetters.length < 25) {
-      localLetters += CHARACTERS.charAt(Math.floor(Math.random() *
-        CHARACTERS.length));
+  const times = (freq: number, letter: string): string[] => {
+    const arr = [];
+    for (let i = 0; i < freq; i++) {
+      arr.push(letter)
     }
-    return localLetters;
+    return arr;
+  }
+  const generateString = (): string => {
+    const ALL_LETTERS_WITH_FREQUENCY = [
+      ...times(56, 'E'), ...times(43, 'A'), ...times(39, 'R'), ...times(38, 'I'), ...times(38, 'O'), ...times(35, 'T'),
+      ...times(34, 'N'), ...times(29, 'S'), ...times(28, 'L'), ...times(23, 'C'), ...times(18, 'U'), ...times(17, 'D'),
+      ...times(16, 'P'), ...times(15, 'M'), ...times(15, 'H'), ...times(13, 'G'), ...times(11, 'B'), ...times(5, 'F'), 
+      ...times(9, 'Y'), ...times(7, 'W'), ...times(6, 'K'), ...times(5, 'V'), ...times(2, 'X'), 'Z', 'J', 'Q'];
+    const shuffled = ALL_LETTERS_WITH_FREQUENCY
+      .map(value => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value)
+    return shuffled.slice(0, 25).reduce((prev, cur) => prev + cur);
   }
 
   useEffect(() => {
     let localLetters = generateString();
     let numVowels = localLetters.replaceAll(/[B-DF-HJ-NP-TV-Z]/g, '').length;
     let numConsonants = localLetters.replaceAll(/[AEIOU]/g, '').length;
-    while(numVowels < 8 || numConsonants < 15) {
+    while(numVowels < 5 || numConsonants < 15) {
+      console.log('Regenerating: ', localLetters)
       localLetters = generateString();
       numVowels = localLetters.replaceAll(/[B-DF-HJ-NP-TV-Z]/g, '').length;
       numConsonants = localLetters.replaceAll(/[AEIOU]/g, '').length;
