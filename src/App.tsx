870a7c988f2c8d86644dba1ad6065d06b58e498f
FettMallows <>
2022-10-08 07:08:01 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+export default function App() {
+  const [letters, setLetters] = useState<string>('');
+  useEffect(() => {
+    let localLetters = '';
+    while (localLetters.length < 25) {
+      localLetters += CHARACTERS.charAt(Math.floor(Math.random() *
+        CHARACTERS.length));
+    }
+    setLetters(localLetters)
+  }, [])
+
+  const renderLetters = () => {
+    const grid = [];
+    for (let i = 0; i < 5; i++) {
+      grid.push()
+    }
+  }
+
+  return (
+    <main>
+      {letters}
+    </main>
+  )
+}
