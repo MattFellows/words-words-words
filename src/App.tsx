074fc14bc0fb7e243ba1574816f8ce325287364a
Matt <gitkraken@matt-fellows.me.uk>
2022-10-09 16:29:38 +0100

    Made submit and check slightly nicer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ export default function App() {
   const [letters, setLetters] = useState<string>('');
   const [word, setWord] = useState<string>('');
   const [chosenLetterIndexes, setChosenLetterIndexes] = useState<number[]>([]);
+  const [bestWord, setBestWord] = useState<string>('')
+  const [finished, setFinished] = useState<boolean>(false)
+  const [success, setSuccess] = useState<boolean>(false)
 
   const times = (freq: number, letter: string): string[] => {
     const arr = [];
@@ -42,10 +45,39 @@ export default function App() {
     return localLetters;
   }
 
+
+  const isWordInLetters = (word: string): boolean => {
+    //console.log(`Is ${word} in ${letters}?`)
+    const localLetters = [...letters];
+    let stillMatching = false;
+    for (let index = 0; index < word.length; index++) {
+      const letter = word.charAt(index).toUpperCase();
+      const matchInd = localLetters.findIndex(l => l === letter);
+      //console.log(`${letter} is index ${matchInd} in ${letters}`)
+      if (matchInd === -1) {
+        return false
+      }
+      stillMatching = true;
+      localLetters.splice(matchInd, 1)
+    }
+    return stillMatching;
+  }
+
   useEffect(() => {
     setLetters(generateWantedString())
   }, [])
 
+  useEffect(() => {
+    if (letters && letters.length === 25) {
+      let best = '';
+      (words as any).words.forEach((w: string) => {
+        if (w.length > best.length && isWordInLetters(w)) {
+          best = w;
+        } 
+      })
+      setBestWord(best);
+    }
+  }, [letters])
 
   const addLetterToWord = (letter: string): void => {
     setWord(word + letter);
@@ -66,30 +98,39 @@ export default function App() {
     return grid;
   }
 
+  const renderFinished = () => (
+    <div className={'finished'}>
+      <div>{`${word} is ${(success ? '' : 'not ')}a word.  The best word the computer found for this grid was: ${bestWord.toUpperCase()}`}</div>
+      <br/>
+      <button onClick={() => {
+        setWord('');
+        setChosenLetterIndexes([])
+        setLetters(generateWantedString())
+        setSuccess(false)
+        setFinished(false)
+      }}>Next Game</button>
+    </div>
+  )
+
   return (
     <>
       <div className='grid'>
-        {renderLetters()}
+        <>
+          {!finished && renderLetters()}
+          {finished && renderFinished()}
+        </>
       </div>
       <div>{word}</div>
-      <div onClick={() => {
+      <button onClick={async () => {
         let foundWord = false;
         (words as any).words.forEach((w: string) => {
-          if (word.toLowerCase() === w.toLowerCase()) {
-            alert('Its a word')
-            setWord('');
-            setChosenLetterIndexes([])
-            setLetters(generateWantedString())
-            foundWord = true;
+          if (word.toLowerCase() === w.toLowerCase()) {           
+            setSuccess(true)
+            return
           }
         })
-        if (!foundWord) {
-          alert('Its not a word that I know')
-          setWord('');
-          setChosenLetterIndexes([])
-          setLetters(generateWantedString())
-        }
-      }}>Submit</div>
+        setFinished(true)
+      }}>Submit</button>
     </>
   )
 }
